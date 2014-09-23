d30d7a713a89e32184bc301c1c85114bb212beb9
jkeifer <jarrettkeifer@gmail.com>
2014-09-23 15:56:40 -0700

    Warnings and tests for source image counts

--- classes.py
+++ classes.py
@@ -1,16 +1,16 @@
 class Metadata:
 	"""
 	An object representing the occurance of one face in a Source Image
-	
+
 	eyes -- dictionary representing the location of the person's eyes in the image (default None)
 		eyes['left' or 'right']['x' or 'y']
-		ex: 
+		ex:
 		> eyes['left']['x']
 		100
 	person -- Person object identifying who the metadata represents (default None)
-	
+
 	"""
-	
+
 	def __init__(self, eyes=None, person=None):
 		self.eyes = eyes
 		self.person = person
@@ -18,12 +18,12 @@ class Metadata:
 class SourceImage:
 	"""
 	One source image containing a photo of one or more faces
-	
+
 	path -- local file path of image (default None)
 	metadatas -- a metadata object representing feature information about a face within the image (default None)
 	image -- PIL image (default None)
 	"""
-	
+
 	def __init__(self, path=None, metadata=None, image=None):
 		self.path = path
 		self.metadata = metadata
@@ -32,10 +32,13 @@ class SourceImage:
 class Person:
 	"""
 	One human individual represented by a collection of source images and a label
-	
+
 	label -- string representing the person's name (default None)
 	"""
-	
-	def __init__(self, label=None):
-		self.label = label
 
+	def __init__(self, label=None, image_count=0):
+		self.label = label
+		self.image_count = image_count
+
+class TrainingError(Exception):
+	pass
--- facetest.py
+++ facetest.py
@@ -1,8 +1,11 @@
 import os, sys, json
+import warnings
 
-from classes import Person, Metadata, SourceImage
+from classes import Person, Metadata, SourceImage, TrainingError
 from image import load_images, rotate_images, crop_images, normalize_images, write_images
 from predict import create_predictive_model, test_model
+
+DEFAULT_MODEL = 'PCA'
 
 def show_usage():
 	print 'Please enter the path of the directory containing the source images and metadata'
@@ -21,6 +24,7 @@ def show_usage():
 	print 'Usage:'
 	print 'python facetest.py ./faceimages/ PCA'
 
+
 def parse_test_type(arg):
 	"""
 	Option to allow different test types
@@ -28,39 +32,39 @@ def parse_test_type(arg):
 	"""
 	if arg != 'PCA' and arg != 'LDA' and arg != 'LBH':
 		show_usage()
-		raise Exception('ERROR: Could not parse test type "%s"' % arg)
+		raise ValueError('Could not parse test type "%s"' % arg)
 		exit()
 	else:
 		return arg
-	
+
 
 def load_people(directory_path):
 	"""
 	Loads images and metadata from a directory into Person, SourceImage, and Metadata objects
 	"""
-	
+
 	#array of person objects
 	training_set = []
-	
+
 	#dictionary to hold all filenames
 	file_dictionaries = {}
 
 	print 'Compiling file list...'
-	
+
 	for eachdirectory, subdirectories, files in os.walk(directory_path):
 		for eachfile in files:
 			if eachdirectory not in file_dictionaries:
 				file_dictionaries[eachdirectory] = []
 			file_dictionaries[eachdirectory].append(eachfile)
-	
+
 	values_count = 0
 	#compile value list
 	for key, value in file_dictionaries.iteritems():
 		values = file_dictionaries[key]
 		values_count += len(values)
-			
+
 	print 'Found %s labels and %s files' % (len(file_dictionaries.keys()), values_count)
-	
+
 	for key, value in file_dictionaries.iteritems():
 		person = Person(label = key)
 		files_for_person = file_dictionaries[key]
@@ -68,6 +72,7 @@ def load_people(directory_path):
 		for eachfile in files_for_person:
 			if eachfile[-3:] == 'jpg' or eachfile[-4:] == 'jpeg' or eachfile[-3:] == 'JPG':
 				file_prefix = eachfile.split('.')[0]
+				person.image_count += 1
 				for eachotherfile in files_for_person:
 					if file_prefix == eachotherfile.split('.')[0] and eachotherfile.split('.')[1] == 'meta':
 						#match found
@@ -83,13 +88,19 @@ def load_people(directory_path):
 						metadata = Metadata(eyes=eyes, person=person)
 						source_image = SourceImage(metadata=metadata, path=os.path.join(key, eachfile))
 						training_set.append(source_image)
-					
+		if person.image_count == 0:
+			warnings.warn("A person has been excluded from this analysis due to lack of source images in the person's directory.")
+
 	print 'training set compiled... %s entries' % len(training_set)
 	return training_set
 
-def run_pca():
-	
+
+def run_analysis(directory_path, model_type='PCA'):
+
 	training_set = load_people(directory_path)
+
+	if len(training_set) < 1:
+		raise TrainingError("No source images were found in the specified directory.")
 
 	load_images(training_set)
 
@@ -99,54 +110,25 @@ def run_pca():
 	crop_images(training_set)
 	write_images(training_set)
 
-	model = create_predictive_model(training_set, model_type='PCA')
+	min_image_count = min([x.metadata.person.image_count for x in training_set])
+
+	if model_type == 'LDA' and min_image_count < 2:
+		warnings.warn("LDA requires at least two images per person. Defaulting to {0}.".format(DEFAULT_MODEL))
+		model_type = DEFAULT_MODEL
+
+	model = create_predictive_model(training_set, model_type=model_type)
 
 	test_model(training_set, model)
 
-def run_lda():
-	training_set = load_people(directory_path)
 
-	load_images(training_set)
+if __name__ == '__main__':
 
-	rotate_images(training_set)
+	try:
+		directory_path = sys.argv[1]
+	except Exception as e:
+		show_usage()
+		exit()
 
-	normalize_images(training_set)
-	crop_images(training_set)
-	write_images(training_set)
+	test_type = parse_test_type(sys.argv[2])
 
-	model = create_predictive_model(training_set, model_type='LDA')
-
-	test_model(training_set, model)
-	
-def run_lbh():
-	training_set = load_people(directory_path)
-
-	load_images(training_set)
-
-	rotate_images(training_set)
-
-	normalize_images(training_set)
-	crop_images(training_set)
-	write_images(training_set)
-
-	model = create_predictive_model(training_set, model_type='LBH')
-
-	test_model(training_set, model)
-	
-try:
-	directory_path = sys.argv[1]
-except Exception as e:
-	show_usage()
-	exit()
-
-test_type = parse_test_type(sys.argv[2])
-
-if test_type == 'PCA':
-	run_pca()
-elif test_type == 'LDA':
-	run_lda()
-elif test_type == 'LBH':
-    run_lbh()
-	
-
-
+	run_analysis(directory_path, model_type=test_type)
